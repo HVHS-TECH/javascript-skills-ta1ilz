1f2c9a229805254c63fc030cb35929d59eef1339
ta1ilz <24638vl@hvhs.school.nz>
2026-04-28 23:56:59 +0000

    Apr 28, 2026, 11:56 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -3,7 +3,7 @@ This is a block comment.
 Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
-console.log("hello")
+console.log("hello");
 
 // This is a single line comment
 
