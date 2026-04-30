6a851876dcc34e0e47ae8dbf64f052d82697b2cc
ta1ilz <24638vl@hvhs.school.nz>
2026-04-30 01:21:19 +0000

    Apr 30, 2026, 1:21 AM

--- t03_maths.js
+++ t03_maths.js
@@ -4,6 +4,22 @@ task 3
 console.log("Running t03_variables.js")
 console.log("hello");
 
+// numbers
+
+console.log(10+5);
+
+// variables and numbers
+
+var myNumber = 10;
+console.log(myNumber + 5)
+
+// only variables
+
+var myNumber = 10;
+var myOtherNumber = 5;
+console.log(myNumber+myOtherNumber)
+
+
 // maths
 
 var num = 10;
