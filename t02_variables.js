6ad7a83abffdc9a77edf7b02e9816aa43efe0d79
ta1ilz <24638vl@hvhs.school.nz>
2026-04-30 00:54:36 +0000

    Apr 30, 2026, 12:54 AM

---
+++ t02_variables.js
@@ -0,0 +1,9 @@
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t02_variables.js")
+console.log("hello");
+
+// This is a single line comment
+
