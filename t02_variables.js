c28c832112081916cb24cc573a59d4da73525d34
ta1ilz <24638vl@hvhs.school.nz>
2026-04-30 01:03:45 +0000

    Apr 30, 2026, 1:03 AM

--- t02_variables.js
+++ t02_variables.js
@@ -7,7 +7,12 @@ console.log("hello");
 // variables
 
 let userName = "vikum";
-
+let Age = "15";
+let pocketMoney = "$10";
+let currentYear = "2026";
 
 // main program
 console.log(userName);
+console.log(Age);
+console.log(pocketMoney);
+console.log(currentYear);
