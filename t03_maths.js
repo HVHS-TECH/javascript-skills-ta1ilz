a573e9844b11ec01f1464126e45d7987501be32d
ta1ilz <24638vl@hvhs.school.nz>
2026-04-30 01:04:29 +0000

    Apr 30, 2026, 1:04 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,18 @@
-/* Paste your code from task 2 here */
+/****************************
+task 2
+****************************/
+console.log("Running t02_variables.js")
+console.log("hello");
+
+// variables
+
+let userName = "vikum";
+let Age = "15";
+let pocketMoney = "$10";
+let currentYear = "2026";
+
+// main program
+console.log(userName);
+console.log(Age);
+console.log(pocketMoney);
+console.log(currentYear);
