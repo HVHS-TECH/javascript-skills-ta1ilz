de549dbbdcc955863e4ed24bd545b14b1183e2df
ta1ilz <24638vl@hvhs.school.nz>
2026-05-01 02:48:22 +0000

    May 1, 2026, 2:48 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -16,7 +16,7 @@ console.log("The same piece of paper has a perimeter of "+perimeter)
 
 // Check, does this result look sensible
 if (perimeter > area){
-    console.log("Yay, your area is larger, this is right for this example")
+    console.log("Yay, your area is larger, this is right for this example");
 }else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
 }
