1dc9d17aa26d6924d65289d6016a7e6f64499283
ta1ilz <24638vl@hvhs.school.nz>
2026-05-01 02:54:24 +0000

    May 1, 2026, 2:54 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -11,8 +11,8 @@ var length = 8;
 var width = 10;
 
 // Display the results
-console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
-console.log("The same piece of paper has a perimeter of "+perimeter)
+console.log("A area of a piece of paper with a length of " + length + ", and a width of " + width + " is " + area);
+console.log("The same piece of paper has a perimeter of " + perimeter);
 
 // Check, does this result look sensible
 if (perimeter > area){
