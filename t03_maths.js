ed1778cc23c945b0595123e56553da236efd6394
ta1ilz <24638vl@hvhs.school.nz>
2026-05-01 02:35:33 +0000

    May 1, 2026, 2:35 AM

--- t03_maths.js
+++ t03_maths.js
@@ -43,3 +43,7 @@ answer = num1+num2;
 console.log("hello vikum");
 console.log("as of 2026 you are 15 years old");
 console.log("you were born in Sri Lanka");
+console.log("in 10 years you will be 25 years old");
+console.log("you have 5 fingers on each hand so you have 10 fingersin total");
+console.log("you spend half of you money, now you have 5 dollarsleft");
+console.log("then you get 3 dollars from your friend, now you have 8 dollars");
