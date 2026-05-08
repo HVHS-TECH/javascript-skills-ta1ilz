a0fec945de1fe2811a54a048717e3c5edf7068ae
ta1ilz <24638vl@hvhs.school.nz>
2026-05-08 02:42:39 +0000

    May 8, 2026, 2:42 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,23 +1,18 @@
-console.log("Task 4 - Debugging");
+/****************************
+task 3
+****************************/
+console.log("Running t03_maths.js")
+console.log("hello");
 
-// Set up the size of the page.
-var length = 8;
-var width = 10;
+// variables
 
-// calculate the area
-var area = length * width;
-console.log("area", area);
-// calculate the perimeter
-var perimeter = 2 * length + 2 * width;
-console.log("perimeter", perimeter);
+let userName = "vikum";
+let Age = "15";
+let pocketMoney = "$10";
+let currentYear = "2026";
 
-// Display the results
-console.log("An area of a piece of paper with a length of " + length + ", and a width of " + width + " is " + area);
-console.log("The same piece of paper has a perimeter of " + perimeter);
-
-// Check, does this result look sensible
-if (area > perimeter){
-console.log("Yay, your area is larger, this is right for this example");
-}else{
-console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
-}
+// main program
+console.log(userName);
+console.log(Age);
+console.log(pocketMoney);
+console.log(currentYear);
