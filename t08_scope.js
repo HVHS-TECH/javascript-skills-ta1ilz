33b44cef45b88a3d2f33d3407dd930ad933e2ceb
ta1ilz <24638vl@hvhs.school.nz>
2026-05-14 01:16:01 +0000

    May 14, 2026, 1:16 AM

--- t08_scope.js
+++ t08_scope.js
@@ -3,7 +3,7 @@ const output = document.getElementById("output");
 /* Read the slides, make a prediction, then uncomment this code.
 
 
-var myVar = 0;
+var myVar = 0 
 output.innerHTML = "Start: "+myVar+"<br>";
 
 functionOne();
