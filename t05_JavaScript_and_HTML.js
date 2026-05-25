4f9f94839fcc3ec9e44151dac07516231c810da1
ta1ilz <24638vl@hvhs.school.nz>
2026-05-25 21:20:36 +0000

    May 25, 2026, 9:20 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,31 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+task 5
+****************************/
+console.log("Running t05_j.js")
+console.log("hello");
+
+// variables
+
+var num1 =10;
+var num2 = 5;
+
+// main program
+var add = num1 + num2;
+console.log("Add:", add);
+var subtract = num1 -num2;
+console.log("Subtract:", subtract);
+var multiply = num1 * num2;
+console.log("Multiply:", multiply);
+var divide = num1 / num2;
+console.log("Divide:", divide);
+
+// more variables
+var userName = "Alice";
+
+// main program
+console.log("Hi Vikum");
+console.log("As of 2025 you are 15 years old");
+console.log("You were brorn in Sri Lanka");
+console.log("You have $100");
+console.log("You spend half your money, now you have $50");
+console.log("Then you get $3, you have $53");
